1f64de2997eda94884b4c1700dacc8ecdeb47bf1
HBPol <hector.bolsa@outlook.com>
2025-11-24 22:32:07 +0000

    Added Lighthouse CI support and documentation

--- web/lighthouse.config.cjs
+++ web/lighthouse.config.cjs
@@ -2,6 +2,9 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 1,
+      startServerCommand: "pnpm start -- --hostname 0.0.0.0 --port 3000",
+      startServerReadyPattern: "started server on",
+      url: ["http://localhost:3000/"],
       settings: {
         formFactor: "desktop",
         screenEmulation: { disabled: true },
@@ -12,6 +15,10 @@ module.exports = {
         "categories:performance": ["error", { minScore: 0.85 }],
         "categories:accessibility": ["error", { minScore: 0.85 }],
       },
+    },
+    upload: {
+      target: "filesystem",
+      outputDir: ".lighthouseci",
     },
   },
 };
